e3392f1261161bf5a9dadab469b7f7da058a4f34
Deceive00 <ryanliem231204@gmail.com>
2024-07-11 20:00:23 +0700

    frontend setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,10 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
